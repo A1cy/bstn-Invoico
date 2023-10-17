579360da50c76cd63cfd96a926a4d38d657af67a
A1cy <a1hvdy@gmail.com>
2023-10-17 16:37:02 +0300

    er

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,12 +16,8 @@
       'display': 'block',
       'width': '1520px', // Simulating a large screen width
       'position': 'absolute', // For capturing content
-      'overflow': 'visible', // Ensure all content is visible
-      // Add any other styles here to override mobile-specific styles
+      'overflow': 'visible' // Ensure all content is visible
     });
-
-    // Force a reflow
-    downloadSection[0].offsetHeight;
 
     var cWidth = downloadSection.width();
     var cHeight = downloadSection.height();
@@ -35,7 +31,7 @@
     html2canvas(downloadSection[0], { allowTaint: true }).then(function (canvas) {
       // Adjust styles for the clone for download
       downloadSection.css({
-        'position': 'absolute', // For download
+        'position': 'relative', // For download
         'top': '-5000px'  // Position it out of view
       });
 
