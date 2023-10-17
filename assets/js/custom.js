360a680e0b120dea97415f28e4c903d959b226f6
A1cy <a1hvdy@gmail.com>
2023-10-17 14:17:43 +0300

    as

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,30 +6,20 @@
 
     var downloadSection = $('#download_section');
 
-    // Remove the class that hides the section
-    downloadSection.removeClass('hidden-for-users');
+    // Store the original styles
+    var originalWidth = downloadSection.css('width');
+    var originalVisibility = downloadSection.css('visibility');
 
-    // Store the original width and set a fixed width for large screen
-    var originalWidth = downloadSection.width();
-    downloadSection.css('width', '1520px'); // Simulating a large screen width
-
-    var cWidth = downloadSection.width();
-    var cHeight = downloadSection.height();
-    var topLeftMargin = 0;
-    var pdfWidth = cWidth + topLeftMargin * 2;
-    var pdfHeight = pdfWidth * 1.5 + topLeftMargin * 2;
-    var canvasImageWidth = cWidth;
-    var canvasImageHeight = cHeight;
-    var totalPDFPages = Math.ceil(cHeight / pdfHeight) - 1;
+    // Apply styles for large screen view
+    downloadSection.css({
+      'width': '1520px',
+      'visibility': 'visible'
+    });
 
     html2canvas(downloadSection[0], { allowTaint: true }).then(function (canvas) {
       var imgData = canvas.toDataURL('image/jpeg', 0.5);
-      var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
-      pdf.addImage(imgData, 'JPG', topLeftMargin, topLeftMargin, canvasImageWidth, canvasImageHeight);
-      for (var i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage(pdfWidth, pdfHeight);
-        pdf.addImage(imgData, 'JPG', topLeftMargin, -(pdfHeight * i) + topLeftMargin * 0, canvasImageWidth, canvasImageHeight);
-      }
+      var pdf = new jsPDF('p', 'pt', [1520, 2280]); // Assuming an aspect ratio similar to A4
+      pdf.addImage(imgData, 'JPG', 0, 0, 1520, canvas.height * (1520 / canvas.width));
 
       var blob = pdf.output('blob');
       var link = document.createElement('a');
@@ -43,9 +33,11 @@
     }).catch(function (error) {
       console.error("Error generating PDF:", error);
     }).finally(function () {
-      // Revert the width back to its original state and hide the section using the class
-      downloadSection.css('width', originalWidth + 'px');
-      downloadSection.addClass('hidden-for-users');
+      // Revert the styles back to their original state
+      downloadSection.css({
+        'width': originalWidth,
+        'visibility': originalVisibility
+      });
     });
   });
 })(jQuery);
