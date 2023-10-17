cefc82ae82254d464d94602eb6a0e578f575d420
A1cy <a1hvdy@gmail.com>
2023-10-17 11:35:45 +0300

    ert

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,45 +14,48 @@
     var originalWidth = downloadSection.width();
     downloadSection.css('width', '1920px');
 
-    var cWidth = downloadSection.width();
-    var cHeight = downloadSection.height();
-    var topLeftMargin = 0;
-    var pdfWidth = cWidth + topLeftMargin * 2;
-    var pdfHeight = pdfWidth * 1.5 + topLeftMargin * 2;
-    var canvasImageWidth = cWidth;
-    var canvasImageHeight = cHeight;
-    var totalPDFPages = Math.ceil(cHeight / pdfHeight) - 1;
+    // Introduce a short delay to allow the browser to apply the CSS changes
+    setTimeout(function() {
+      var cWidth = downloadSection.width();
+      var cHeight = downloadSection.height();
+      var topLeftMargin = 0;
+      var pdfWidth = cWidth + topLeftMargin * 2;
+      var pdfHeight = pdfWidth * 1.5 + topLeftMargin * 2;
+      var canvasImageWidth = cWidth;
+      var canvasImageHeight = cHeight;
+      var totalPDFPages = Math.ceil(cHeight / pdfHeight) - 1;
 
-    html2canvas(downloadSection[0], { allowTaint: true }).then(function (
-      canvas
-    ) {
-      canvas.getContext('2d');
-      var imgData = canvas.toDataURL('image/jpeg', 1.0);
-      var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
-      pdf.addImage(
-        imgData,
-        'JPG',
-        topLeftMargin,
-        topLeftMargin,
-        canvasImageWidth,
-        canvasImageHeight
-      );
-      for (var i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage(pdfWidth, pdfHeight);
+      html2canvas(downloadSection[0], { allowTaint: true }).then(function (
+        canvas
+      ) {
+        canvas.getContext('2d');
+        var imgData = canvas.toDataURL('image/jpeg', 1.0);
+        var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
         pdf.addImage(
           imgData,
           'JPG',
           topLeftMargin,
-          -(pdfHeight * i) + topLeftMargin * 0,
+          topLeftMargin,
           canvasImageWidth,
           canvasImageHeight
         );
-      }
-      pdf.save('MHG-Sales-invoice.pdf');
+        for (var i = 1; i <= totalPDFPages; i++) {
+          pdf.addPage(pdfWidth, pdfHeight);
+          pdf.addImage(
+            imgData,
+            'JPG',
+            topLeftMargin,
+            -(pdfHeight * i) + topLeftMargin * 0,
+            canvasImageWidth,
+            canvasImageHeight
+          );
+        }
+        pdf.save('MHG-Sales-invoice.pdf');
 
-      // Revert the width back to its original state and hide the section
-      downloadSection.css('width', originalWidth + 'px');
-      downloadSection.css('display', 'none');
-    });
+        // Revert the width back to its original state and hide the section
+        downloadSection.css('width', originalWidth + 'px');
+        downloadSection.css('display', 'none');
+      });
+    }, 100); // 100ms delay
   });
 })(jQuery); // End of use strict
