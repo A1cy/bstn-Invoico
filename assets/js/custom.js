39a92f1e20dc2c7a94b97b09b468d05e92146b4c
A1cy <a1hvdy@gmail.com>
2023-10-18 10:42:00 +0300

    df

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@
   $('#generatePDF').on('click', function (event) {
     event.preventDefault();
 
-    var renderingWidth = 41;
+    var renderingWidth = 49;
 
     var downloadSection = $('#download_section').clone();
     $('body').append(downloadSection);
