d722b31162fcbe4aca9ec8bf13feb72a8fbddd46
A1cy <a1hvdy@gmail.com>
2023-10-18 16:49:42 +0300

    gg

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,3 @@
-
 // Detect mobile Chrome browser
 function isMobileChrome() {
   return /Android.*Chrome/.test(navigator.userAgent);
@@ -13,71 +12,64 @@ function isSafari() {
   'use strict';
 
   $('#generatePDF').on('click', function (event) {
-    event.preventDefault();
+      event.preventDefault();
 
-    event.preventDefault();
+      var fixedWidth = 1024;  // This represents a typical desktop width
+      var downloadSection = $('#download_section').clone();
+      $('body').append(downloadSection);
+      $('#download_section').hide();
 
-    // Set a fixed rendering width to represent a large screen view
-    var fixedWidth = 1024;  // This represents a typical desktop width
-    var deviceWidth = $(window).width();
-    var scaleRatio = deviceWidth / fixedWidth;
+      downloadSection.css({
+          'display': 'block',
+          'width': fixedWidth + 'px',  // Set to fixed width
+          'position': 'relative',
+          'overflow': 'visible'
+      });
 
-    var downloadSection = $('#download_section').clone();
-    $('body').append(downloadSection);
-    $('#download_section').hide();
+      var cWidth = downloadSection.width();
+      var cHeight = downloadSection.height();
+      var topLeftMargin = 0;
+      var pdfWidth = cWidth + topLeftMargin * 2;
+      var pdfHeight = cHeight + topLeftMargin * 2;
 
-    downloadSection.css({
-      'display': 'block',
-      'width': fixedWidth + 'px',  // Set to fixed width
-      'transform': 'scale(' + scaleRatio + ')',  // Scale the content to fit the device width
-      'transform-origin': 'top left',
-      'position': 'relative',
-      'overflow': 'visible'
-    });
-
-    var cWidth = downloadSection.width();
-    var cHeight = downloadSection.height();
-    var topLeftMargin = 0;
-    var pdfWidth = cWidth + topLeftMargin * 2;
-    // Set the pdfHeight dynamically based on content height, while maintaining the width-to-height ratio
-    var pdfHeight = cHeight + topLeftMargin * 2;
-
-    
-    if (isMobileChrome()) {
-      pdfWidth *= 0.8;  // Adjusting width for mobile Chrome
-      pdfHeight *= 0.8; // Adjusting height for mobile Chrome
-    }
-
-    var totalPDFPages = Math.ceil(cHeight / pdfHeight) - 1;
-
-    html2canvas(downloadSection[0], { allowTaint: true }).then(function (canvas) {
-      var imgData = canvas.toDataURL('image/jpeg', 0.7);
-      var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
-      
-    // Dynamic scaling based on actual width and height
-    var scale_factor = Math.min(pdfWidth / cWidth, pdfHeight / cHeight);
-    cWidth *= scale_factor;
-    cHeight *= scale_factor;
-
-    pdf.addImage(imgData, 'JPG', topLeftMargin, topLeftMargin, cWidth, cHeight);
-      for (var i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage(pdfWidth, pdfHeight);
-        pdf.addImage(imgData, 'JPG', topLeftMargin, -(pdfHeight * i) + topLeftMargin, cWidth, cHeight);
+      if (isMobileChrome()) {
+          pdfWidth *= 0.8;  // Adjusting width for mobile Chrome
+          pdfHeight *= 0.8; // Adjusting height for mobile Chrome
       }
 
-      var dataURI = pdf.output('datauristring');
-    // Safari specific download handling
-    if (isSafari()) {
-      var blob = pdf.output('blob');
-      var blobURL = window.URL.createObjectURL(blob);
-      window.location.href = blobURL;
-      return;
-    }
-    
-    
-      var link = document.createElement('a');
-      link.href = dataURI;
-      link.download = 'MHG-Sales-invoice.pdf';
+      var totalPDFPages = Math.ceil(cHeight / pdfHeight) - 1;
+
+      html2canvas(downloadSection[0], {
+          allowTaint: true,
+          scale: 2  // Increase resolution
+      }).then(function (canvas) {
+          var imgData = canvas.toDataURL('image/jpeg', 1.0);  // Set maximum quality
+          var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
+
+          // Dynamic scaling based on actual width and height
+          var scale_factor = Math.min(pdfWidth / cWidth, pdfHeight / cHeight);
+          cWidth *= scale_factor;
+          cHeight *= scale_factor;
+
+          pdf.addImage(imgData, 'JPG', topLeftMargin, topLeftMargin, cWidth, cHeight);
+          for (var i = 1; i <= totalPDFPages; i++) {
+              pdf.addPage(pdfWidth, pdfHeight);
+              pdf.addImage(imgData, 'JPG', topLeftMargin, -(pdfHeight * i) + topLeftMargin, cWidth, cHeight);
+          }
+
+          var dataURI = pdf.output('datauristring');
+
+          // Safari specific download handling
+          if (isSafari()) {
+              var blob = pdf.output('blob');
+              var blobURL = window.URL.createObjectURL(blob);
+              window.location.href = blobURL;
+              return;
+          }
+
+          var link = document.createElement('a');
+          link.href = dataURI;
+          link.download = 'MHG-Sales-invoice.pdf';
       document.body.appendChild(link);
       link.click();
  
